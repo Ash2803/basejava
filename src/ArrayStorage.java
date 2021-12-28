17c61fec53a61919fd5b9e1c86d0cf4944db83fd
Ash <ashot.93@mail.ru>
2021-12-28 23:58:06 +0300

    Updated methods for Resumes

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,42 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        Arrays.fill(storage,0, size(), null);
     }
 
-    void save(Resume r) {
+    void save(Resume r){
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for(int i = 0; i < storage.length; i++) {
+            if(storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
